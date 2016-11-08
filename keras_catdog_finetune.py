4929ee496a822e9b2d0981f17bf4b607d42953ad
maxlz <maxlz@users.noreply.github.com>
2016-11-08 20:09:58 +0800

    1
    1

--- keras_catdog_finetune.py
+++ keras_catdog_finetune.py
@@ -25,7 +25,7 @@ def load_weights(weights_path,model):
     assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
     f = h5py.File(weights_path)
     for k in range(f.attrs['nb_layers']):
-        if k >= 25:
+        if k >= len(model.layers):
             # we don't look at the last (fully-connected) layers in the savefile
             break
         g = f['layer_{}'.format(k)]
@@ -90,11 +90,12 @@ model = VGG_16("/home/max/下载/vgg16_weights.h5")
 #datagen = ImageDataGenerator(rescale=1./255)
 datagen = ImageDataGenerator()
 
+#注意这里的batch_size=32,跟下面的top fit对应，否则无法训练top
 train_data = datagen.flow_from_directory("/home/max/data/train",target_size=(224,224)\
-                                         ,shuffle=False,class_mode=None)
+                                         ,batch_size=32,shuffle=False,class_mode=None)
 
 test_data = datagen.flow_from_directory("/home/max/data/test",target_size=(224,224)\
-                                         ,shuffle=False,class_mode=None)
+                                         ,batch_size=32,shuffle=False,class_mode=None)
 
 train_out = model.predict_generator(train_data,val_samples=2000)
 
@@ -126,20 +127,42 @@ def build_top():
     
 top_model = build_top()
 
+#到此，实验2完成，top训练达到90%
+
+
 model.add(top_model)
 
+#冻结前25层
 for layer in model.layers[:25]:
     layer.trainable = False
     
 model.compile(optimizer=SGD(lr=1e-4,momentum=0.9),loss='binary_crossentropy',metrics=['accuracy'])
 
 
-train_data.reset()
-test_data.reset()
+#重新初始化训练集生成器
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
-train_data.class_mode = 'binary'
-test_data.class_mode = 'binary'
+test_datagen = ImageDataGenerator(rescale=1./255)
 
-model.fit_generator(generator=train_data,samples_per_epoch=800,nb_epoch=50,validation_data=test_data,nb_val_samples=400)
-    
+train_generator = train_datagen.flow_from_directory(
+        "/home/max/data/train/",
+        target_size=(224,224),
+        batch_size=32,
+        class_mode='binary')
 
+validation_generator = test_datagen.flow_from_directory(
+        "/home/max/data/test/",
+        target_size=(224,224),
+        batch_size=32,
+        class_mode='binary')
+#至此，完成实验3，精度达到95%
+model.fit_generator(
+        train_generator,
+        samples_per_epoch=2000,
+        nb_epoch=50,
+        validation_data=validation_generator,
+        nb_val_samples=800)
